1c8a0f7b98045b17cd9b3bb12355fa72e89d2b45
Panuthep <falcon_270394@hotmail.co.th>
2024-06-12 15:28:21 +0700

    init

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+# with open("requirements.txt") as f:
+#     dependencies = [line for line in f]
+
+setup(
+    name='kalm',
+    version='0.1',
+    packages=find_packages("src"),
+    package_dir={'': 'src'},
+    license='Apache-2.0 License',
+    author='Panuthep Tasawong',
+    author_email='panuthep.t_s20@vistec.ac.th',
+    description='A Universal Framework for Knowledge-Augmented Language Model (KALM) Applications',
+    python_requires='>=3.11',
+    # install_requires=dependencies
+)
