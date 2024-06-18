59767bab3c6babf0cb36dbd360c114c7f94747a9
Panuthep <falcon_270394@hotmail.co.th>
2024-06-18 21:13:39 +0700

    update code

--- src/okean/modules/entity_linking/baseclass.py
+++ src/okean/modules/entity_linking/baseclass.py
@@ -1,5 +1,12 @@
+from abc import ABC
+from typing import List
+from okean.data_types.basic_types import Doc
 
 
-
-class BaseEntityLinking:
-    pass
+class BaseEntityLinking(ABC):
+    def __call__(
+            self, 
+            texts: List[str]|str = None, 
+            docs: List[Doc]|Doc = None,
+    ) -> List[Doc]:
+        pass
