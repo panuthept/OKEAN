08c193fe6809df350374fee7ab612cc3640e615a
Panuthep <falcon_270394@hotmail.co.th>
2024-06-27 22:24:00 +0700

    update code

--- src/okean/modules/entity_linking/elq.py
+++ src/okean/modules/entity_linking/elq.py
@@ -239,7 +239,8 @@ class ELQ(EntityLinking):
                 print(f"combined_scores:\n{combined_scores}\n{combined_scores.size()}")
 
                 # (batch_size, num_pred_mentions)
-                pred_mention_masks = (mention_logits > 0)
+                pred_mention_masks = (mention_logits > 0).reshape(mention_logits.size(0), -1)
+                print(f"pred_mention_masks:\n{pred_mention_masks}\n{pred_mention_masks.size()}")
                 pred_mention_bounds = mention_bounds[pred_mention_masks]
                 pred_combined_scores = combined_scores[pred_mention_masks]
                 # (batch_size, num_pred_mentions, max_candidates)
