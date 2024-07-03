9871defa7d7ddd866a496c309478ff12753ee643
Panuthep <falcon_270394@hotmail.co.th>
2024-07-03 11:49:05 +0700

    add constant variables for dr.py

---
+++ src/okean/modules/retrieval/const.py
@@ -0,0 +1,35 @@
+AVAILABLE_PRETRAINED_IR_MODELS = {
+  "E5": [
+    "intfloat/multilingual-e5-small",
+    "intfloat/multilingual-e5-base",
+    "intfloat/multilingual-e5-large",
+    "intfloat/multilingual-e5-large-instruct",
+    "intfloat/e5-small",
+    "intfloat/e5-base",
+    "intfloat/e5-large",
+    "intfloat/e5-small-v2",
+    "intfloat/e5-base-v2",
+    "intfloat/e5-large-v2",
+  ],
+  "MPNet": [
+      "sentence-transformers/all-mpnet-base-v2"
+  ],
+  "BGEm3": [
+    "BAAI/bge-m3",
+  ],
+}
+
+DENSE_RETRIEVAL_CONFIGS = {
+    "E5": {
+        "max_query_length": 512,
+        "max_passage_length": 512,
+        "pooling_strategy": "average_pooling",
+        "add_prefix": True,
+    },
+    "Default": {
+        "max_query_length": 512,
+        "max_passage_length": 512,
+        "pooling_strategy": "average_pooling",
+        "add_prefix": False,
+    },
+}
